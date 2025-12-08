833385c0178de4f105d94e3aae4cccd74da00538
ocerlis <56587621+Ocerlis@users.noreply.github.com>
2025-12-08 12:01:01 +0600

    Added basic layout for app

--- kidthing/ContentView.swift
+++ kidthing/ContentView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var firstTable = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @State private var secondTable = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What is")
+            Text("2 x 2?").fontWeight(.bold).padding()
+            
+            Button("Test1") { }.font(.title2).frame(width: 200, height: 100).background(.red).foregroundStyle(.white).clipShape(RoundedRectangle(cornerRadius: 5))
+            Button("Test2") { }.font(.title2).frame(width: 200, height: 100).background(.blue).foregroundStyle(.white).clipShape(RoundedRectangle(cornerRadius: 5))
+            Button("Test3") { }.font(.title2).frame(width: 200, height: 100).background(.yellow).foregroundStyle(.white).clipShape(RoundedRectangle(cornerRadius: 5))
+            Button("Test3") { }.font(.title2).frame(width: 200, height: 100).background(.green).foregroundStyle(.white).clipShape(RoundedRectangle(cornerRadius: 5))
+            
         }
-        .padding()
     }
 }
 
