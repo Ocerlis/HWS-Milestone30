3a4379842bc9af9dbf6b1cd6e6bc8fca3dcd2bd2
ocerlis <56587621+Ocerlis@users.noreply.github.com>
2025-12-08 10:59:33 +0600

    Initial Commit

---
+++ kidthing/kidthingApp.swift
@@ -0,0 +1,17 @@
+//
+//  kidthingApp.swift
+//  kidthing
+//
+//  Created by Андрей Поль on 08.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct kidthingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
